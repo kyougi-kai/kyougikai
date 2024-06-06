12c6eb57513648230944fdb5add8271cfbbb71ed
merg-zh <66412416+merg-zh@users.noreply.github.com>
2024-06-06 17:46:04 +0900

    yyi

---
+++ KYOUGIKAI/kyougikai/src/test/java/kyougikai/com/kyougikai/KyougikaiApplicationTests.java
@@ -0,0 +1,13 @@
+package kyougikai.com.kyougikai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyougikaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
