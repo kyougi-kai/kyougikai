27bb8d5d8ea33ef33b6fe1d1ecb797af96c6609f
merg-zh <pb.kyougikai@gmail.com>
2024-07-05 13:43:20 +0900

    up

--- test-login222/demo/src/test/sinki.js
+++ test-login222/demo/src/test/sinki.js
@@ -1,25 +1,25 @@
 function pushHideButton() {
-    var txtPass = document.getElementById("textPassword");
-    var btnEye = document.getElementById("buttonEye");
-    if (txtPass.type === "text") {
-      txtPass.type = "password";
-      btnEye.className = "fa fa-eye";
-    } else {
-      txtPass.type = "text";
-      btnEye.className = "fa fa-eye-slash";
-    }
+  var txtPass = document.getElementById("textPassword");
+  var btnEye = document.getElementById("buttonEye");
+  if (txtPass.type === "text") {
+    txtPass.type = "password";
+    btnEye.className = "fa fa-eye";
+  } else {
+    txtPass.type = "text";
+    btnEye.className = "fa fa-eye-slash";
   }
-  function check() {
-    const name = document.getElementById("name");
-    const pass = document.getElementById("textPassword");
+}
+function check() {
+  const name = document.getElementById("name");
+  const pass = document.getElementById("textPassword");
 
-    if(name.value && name.value.length && pass.value && pass.value.length) {
-      if(pass.value.length >= 8){
-        button.style.pointerEvents = "auto";
-        document.getElementById('button').style.backgroundColor = '#006eff';
-      }
-    } else {
-      button.style.pointerEvents = "none";
-      document.getElementById('button').style.backgroundColor = 'rgb(179, 224, 255)';
+  if (name.value && name.value.length && pass.value && pass.value.length) {
+    if (pass.value.length >= 8) {
+      button.style.pointerEvents = "auto";
+      document.getElementById('button').style.backgroundColor = '#006eff';
     }
+  } else {
+    button.style.pointerEvents = "none";
+    document.getElementById('button').style.backgroundColor = 'rgb(179, 224, 255)';
   }
+}
