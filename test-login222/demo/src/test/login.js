6c93db98893dfaed0aead345b32189b63d27bc2b
merg-zh <pb.kyougikai@gmail.com>
2024-07-05 15:20:34 +0900

    up

--- test-login222/demo/src/test/login.js
+++ test-login222/demo/src/test/login.js
@@ -1,9 +1,23 @@
-function checkname() {
+function pushHideButton() {
+  var txtPass = document.getElementById("textPassword");
+  var btnEye = document.getElementById("buttonEye");
+  if (txtPass.type === "text") {
+    txtPass.type = "password";
+    btnEye.className = "fa fa-eye";
+  } else {
+    txtPass.type = "text";
+    btnEye.className = "fa fa-eye-slash";
+  }
+}
+function check() {
   const name = document.getElementById("name");
+  const pass = document.getElementById("textPassword");
 
-  if (name.value && name.value.length) {
-    button.style.pointerEvents = "auto";
-    document.getElementById('button').style.backgroundColor = '#006eff';
+  if (name.value && name.value.length && pass.value && pass.value.length) {
+    if (pass.value.length >= 8) {
+      button.style.pointerEvents = "auto";
+      document.getElementById('button').style.backgroundColor = '#006eff';
+    }
   } else {
     button.style.pointerEvents = "none";
     document.getElementById('button').style.backgroundColor = 'rgb(179, 224, 255)';
