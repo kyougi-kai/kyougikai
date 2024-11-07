971a8b53be3c78ebd8b5ad185487493190437621
merg-hz <pb.kyougikai@gmail.com>
2024-11-07 18:03:03 +0900

    up

--- kyougikai.com/testbynagaya.php
+++ kyougikai.com/testbynagaya.php
@@ -7,7 +7,6 @@
 </head>
 <body>
 asd
-
     <?php
 $link = mysqli_connect("localhost","root","","test");
 
@@ -17,7 +16,8 @@ $result = mysqli_query($link, "SELECT * FROM question");
 $result = $result->fetch_all(MYSQLI_ASSOC);
 
 echo $result[0]["種別"];
-
+$img_src = base64_encode($result[0]["画像"]);
+echo '<img src="data:image/jpeg;base64,' . $img_src . '" alt="BLOB Image"width="300" height="200" />';
 mysqli_close($link);
 
 ?>
